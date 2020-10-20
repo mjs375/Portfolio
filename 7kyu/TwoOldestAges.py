ed2b12f162fb322614cf0701f591e75d920b5ce4
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-20 14:23:55 -0400

    Update TwoOldestAges.py

--- 7kyu/TwoOldestAges.py
+++ 7kyu/TwoOldestAges.py
@@ -14,3 +14,9 @@ The order of the numbers passed in could be any order. The array will always inc
 For example:
     two_oldest_ages([1, 3, 10, 0]) # should return [3, 10]
 """
+
+# # # # # # # # # # B E S T _ P R A C T I C E : # # # # # # # # # # # # # #
+def two_oldest_ages(ages):
+    return sorted(ages)[-2:]
+    #Python slice notation:
+        # '-2:' means start 2 from the end (thus the two oldest), to the end. Brilliant!
