3db2d2c6f793dceba0e59504f0747a46d8eb0cf6
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-21 13:47:17 -0400

    Create SuperDuperEasy.py

---
+++ 8kyu/SuperDuperEasy.py
@@ -0,0 +1,10 @@
+def problem(a):
+    try:
+        a = (a*50)+6
+        return a
+    except:
+        return "Error"
+    
+"""
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+"""
