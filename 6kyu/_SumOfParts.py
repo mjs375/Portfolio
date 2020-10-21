80a63f7595e2ff7b59d19a2e3dcc558cba5cbc64
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-21 19:10:00 -0400

    Create _SumOfParts.py

---
+++ 6kyu/_SumOfParts.py
@@ -0,0 +1,28 @@
+#
+# # This code 'works', but not for some test arrays (that have 200,000+ items). Need to optimize!
+#
+
+def parts_sums(ls):
+    temp = sum(ls)
+    sums = [temp]
+    for i in range(len(ls)):
+        delete = ls.pop(0) 
+        temp = temp - delete
+        sums.append(temp)
+    return sums
+    
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+"""
+Let us consider this example (array written in general format):
+  ls = [0, 1, 3, 6, 10]
+Its following parts:
+  ls = [0, 1, 3, 6, 10]
+  ls = [1, 3, 6, 10]
+  ls = [3, 6, 10]
+  ls = [6, 10]
+  ls = [10]
+  ls = []
+The corresponding sums are (put together in a list): [20, 20, 19, 16, 10, 0].
+The function parts_sums (or its variants in other languages) will take as parameter a list ls,
+and return a list of the sums of its parts as defined above.
+"""
