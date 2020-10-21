c499174215394c0a09e2fb7d9549726bfad65be4
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-21 13:38:01 -0400

    Create InvertValues.py

---
+++ 8kyu/InvertValues.py
@@ -0,0 +1,6 @@
+def invert(lst):
+    return [(i*-1) for i in lst]
+
+"""
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+"""
