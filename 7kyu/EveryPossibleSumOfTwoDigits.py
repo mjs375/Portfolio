25b2ccbf01dae7ecb7d7c97b426018b0c4ee03fb
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-20 20:40:48 -0400

    Update EveryPossibleSumOfTwoDigits.py

--- 7kyu/EveryPossibleSumOfTwoDigits.py
+++ 7kyu/EveryPossibleSumOfTwoDigits.py
@@ -15,3 +15,14 @@ E.g.:
     # combinations('ABCD',2) --> AB AC AD BC BD CD
     # combinations(range(4), 3) --> 012 013 023 123
 """
+
+"""
+Given a long number, return all the possible sum of two digits of it.
+
+For example, 12345: all possible sum of two digits from that number are:
+
+[ 1 + 2, 1 + 3, 1 + 4, 1 + 5, 2 + 3, 2 + 4, 2 + 5, 3 + 4, 3 + 5, 4 + 5 ]
+Therefore the result must be:
+
+[ 3, 4, 5, 6, 5, 6, 7, 7, 8, 9 ]
+"""
