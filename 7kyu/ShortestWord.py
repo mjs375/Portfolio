3ba304e748947580db261847f5088b454bd0096e
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-20 20:51:09 -0400

    Update ShortestWord.py

--- 7kyu/ShortestWord.py
+++ 7kyu/ShortestWord.py
@@ -1,4 +1,17 @@
 def find_short(s):
+    return min(len(word) for word in s.split(" "))
+
+"""
+1. Split() 's' into a list, separated by " " (default is also whitespace).
+2. Loop: for word in [new split 's' LIST]
+3. [Make a list of] len(word) done in each loop
+4. Find the min of that list:
+ - the shortest word
+"""
+
+# # # # # # # # # # F I R S T _ A T T E M P T : # # # # # # # # # # # # # 
+
+def find_short(s):
     list = s.split(" ") #split 's' into a list, separating by whitespace
     l = "" #shortest word length
     for word in list:
