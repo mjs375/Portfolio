435e48706e87b312a95705bbb234c5af694c4e08
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-21 13:29:10 -0400

    Update SetAlarm.py

--- 8kyu/SetAlarm.py
+++ 8kyu/SetAlarm.py
@@ -7,3 +7,8 @@ def set_alarm(employed, vacation):
 """
 Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation. The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. 
 """
+
+# # # # # # # # B E S T _ P  R A C T I C E : # # # # # # # #
+
+def set_alarm(employed, vacation):
+    return employed and not vacation
