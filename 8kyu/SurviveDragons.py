0d2ff6923632952dfbe9883a79581479f990364a
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-21 20:10:28 -0400

    Update SurviveDragons.py

--- 8kyu/SurviveDragons.py
+++ 8kyu/SurviveDragons.py
@@ -5,3 +5,6 @@ def hero(bullets, dragons):
 A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
 Return True if yes, False otherwise :)    
 """
+# # # # # # B E S T : # # # # # # 
+def hero(bullets, dragons):
+    return bullets >= dragons * 2 #returns the boolean value evaluated from the statement
