90b30a139c73777d2a8d4d6f6ec035589fe9d2a1
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-21 22:14:36 -0400

    Create RepeatString.py

---
+++ 8kyu/RepeatString.py
@@ -0,0 +1,9 @@
+def repeat_str(repeat, string):
+    return (string)+(string)*(repeat-1)
+
+
+
+"""
+Write a function called repeat_str which repeats the given string src exactly count times.
+    repeat_Str(6, "I") // "IIIIII"
+"""
