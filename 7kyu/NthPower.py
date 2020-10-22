995cae3a21d557e40c9ab1ab919b5db9dc6affa3
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-21 20:37:54 -0400

    Create NthPower.py

---
+++ 7kyu/NthPower.py
@@ -0,0 +1,17 @@
+def modified_sum(a, n):
+    sums = int()
+    initial = sum(a)
+    for num in a:
+        sums += num**n
+    #for num in a: sum(sum,num**n)
+    return sums - initial 
+
+"""
+You are provided with array of positive non-zero ints and int n representing n-th power (n >= 2).
+
+For the given array, calculate the sum of each value to the n-th power. Then subtract the sum of the original array.
+
+Example 1: Input: {1, 2, 3}, 3 --> (1 ^ 3 + 2 ^ 3 + 3 ^ 3 ) - (1 + 2 + 3) --> 36 - 6 --> Output: 30
+
+Example 2: Input: {1, 2}, 5 --> (1 ^ 5 + 2 ^ 5) - (1 + 2) --> 33 - 3 --> Output: 30
+"""
