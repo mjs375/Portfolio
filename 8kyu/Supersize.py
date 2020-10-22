1a0da1f6ba6a549b682cd7e74b428022d55a8431
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-21 21:21:02 -0400

    Create Supersize.py

---
+++ 8kyu/Supersize.py
@@ -0,0 +1,10 @@
+def super_size(n):
+    return int("".join(sorted(str(n), reverse=True)))
+
+"""
+Write a function that rearranges an integer into its largest possible value.
+    super_size(123456) # 654321
+    super_size(105)    # 510
+    super_size(12)     # 21
+If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.
+"""
