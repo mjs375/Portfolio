221f2d56ad2337e023afa355c0b8adc0dee10383
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-24 14:27:29 -0400

    Update GeneratingMarkdown.py

--- 7kyu/GeneratingMarkdown.py
+++ 7kyu/GeneratingMarkdown.py
@@ -7,6 +7,38 @@ def generate_markdowns(markdown, text, url_or_language): # all strings
         print(markdown, "|*|", text, "|*|", url_or_language)
         return f"```{url_or_language}\n{text}\n```"
 
+    
+# # # # # B E T T E R : # # # # # #
+pattern = {
+    'link': '[{txt}]({url_or_language})',
+    'img':  '![{txt}]({url_or_language})',
+    'code': "```{url_or_language}\n{txt}\n```",
+} 
+# dict of models:
+    # key is markdown-type (link, img, code)
+    # value is format of return
+
+def generate_markdowns(markdown, txt, url_or_language):
+    return pattern[markdown].format(txt=txt, url_or_language=url_or_language)
+    # above asks for the dict key
+        # formatted a
+        
+        
+""" 
+# Getting a Dict Value, where its value is a format string (feed it variables to insert to pattern)
+    numberFormat = 'Count in Spanish: {one}, {two}, {three}, ...'
+    withSubstitutions = numberFormat.format(one='uno', two='dos', three='tres')
+    print(withSubstitutions)
+# .format()
+    txt = "For only {price:.2f} dollars!"
+    print(txt.format(price = 49))
+"""
+
+
+
+
+
+
 
 
 """
