1ec220eb7db2ec7690f2b3e32fcbddfde22149d1
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-24 11:42:00 -0400

    Update ***QuidditchScoreboard.py

--- 6kyu/***QuidditchScoreboard.py
+++ 6kyu/***QuidditchScoreboard.py
@@ -1,3 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # # # # # # # # # # # # # # # # ## # # # # # # # # # # # # # # # # # # # # # # # 
+"""
+A LESSON IN FAILURE:
+Originally I actually intentionally tried a more complicated-than-necessary approach,
+in order to practice with iterating lists & dictionaries, because I uncommonly did that.
+BUT... the code worked perfectly (if complicatedly), except for 1 thing: it wouldn't read
+into the dict all the 'plays' of the game. Why? Because the dict was set up as team_name=key
+and play=value. In a dict, each key must be distinct! This method would never have worked, b/c
+you couldn't store a team as a key multiple times. 
+"""
+
 def quidditch_scoreboard(teams, actions):
     ## 1. GET TEAM_NAMES/SCORE DICT
     team_list = teams.split(" vs ") # ['Kenmare Kestrels', 'Barnton']
@@ -7,15 +31,9 @@ def quidditch_scoreboard(teams, actions):
     actions = actions.replace(", ","|") 
     print(actions)
     #
-    
-    
     #game = dict(kv.split(": ") for kv in actions.split("|")) #creates kv pairs of team:action, team:action in DICT
     # CAN'T USE TEAM NAMES AS KEYS BECAUSE MULTIPLE KEYS CAN'T HAVE THE SAME NAME DUMBASS
-   
-
-    
-    
-    
+    #
     print(game) 
     ## 3. Iterate through 'game'
     #game_items = game.items() #[(key, value), (key, value), ...] Creates iterable dict
@@ -38,13 +56,7 @@ def quidditch_scoreboard(teams, actions):
     #Remove trailing ", " on 2nd team:
     outcome = outcome[0:-2] #remove last 2 elem of string
     return outcome
-
-
-
-
-
-
-
+# # # # 
 def pts(game_dict, team, points): #scoreboard, scoring_team, score(+-)
     #ex_dict["key"] = value #How to update a dictionary
     oldscore = game_dict[team]
@@ -52,11 +64,4 @@ def pts(game_dict, team, points): #scoreboard, scoring_team, score(+-)
     game_dict[team] = newscore
     #print(game_dict)
     return game_dict
-
-
-
-
-
-
-
 
