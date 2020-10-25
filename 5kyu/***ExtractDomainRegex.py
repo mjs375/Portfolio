c6da14cad0dc420093ecbf0f691813e610a5b12a
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-24 22:22:09 -0400

    Update ***ExtractDomainRegex.py

--- 5kyu/***ExtractDomainRegex.py
+++ 5kyu/***ExtractDomainRegex.py
@@ -1,14 +1,28 @@
 import re
 
 def domain_name(url): #string
+    print(url)
     #match = re.search("h(tt)p",url)
     #print(match.groups())
-    match = re.search("https://([A-Za-z_0-9].*)",url)
-    print(match.groups())
+    match = re.search("(?:http://www.|http://|www.|https://|^)([A-Za-z0-9-]*)(?:\.)",url) #EXPLANATION BELOW
+    print(match.group(0))
+    #print(match.group(1))
+    #print(match.group(2))
+    return match.group(1)
+
+#Explanation of Regex(^):
+    # (?:  )   # a way to group, a 'non'-capture group/version: turns off its 'capturedness'
+    # http|www|...  # a set of substrings to search for, separated by the | which means 'or'
+    # ^     # the last |^ in the noncapture group means, just start at the beginning of the entire text (for websites with no preceding www.)
+    # ([A-Za-z0-9-]*)   #the content we actually want: a group of alpha-numeric-dash letters (*any amount)
+    #(\.)   #the \ 'escapes' the period, so it is an actual period, not interpreted as a regex expression
     
     
 """ PYTHON   R E G E X :
-.group()    Returns string matched by the RE
+.group()    Returns string matched by the RE. Use capture groups (...) in the regex.
+    >> result.group(1) will return 1st capture group, ie stuff within (...)
+    >> result.group(0) will return the entire_matched_text
+.groups()
 .start()    Returns starting position of match
 .end()      Returns ending position of match
 .span()     Returns a tuple containing (start, end) of match.
@@ -17,9 +31,11 @@ def domain_name(url): #string
 r"..."      r-prefix means the following is a raw string, ie backslash characters are treated literally instead of signifying special treatment
     >'\n'    a single newline
     >r'\n'   2 characters: \  n
-
-
 """
+
+
+
+
 
 """ EXTRACT THE DOMAIN NAME FROM A URL:
 Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
