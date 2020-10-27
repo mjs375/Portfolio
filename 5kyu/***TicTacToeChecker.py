34c66303bd830ae9c5e0c9df6dc47aeb3b2ed804
MJ <66171297+mjs375@users.noreply.github.com>
2020-10-26 20:43:32 -0400

    Update ***TicTacToeChecker.py

--- 5kyu/***TicTacToeChecker.py
+++ 5kyu/***TicTacToeChecker.py
@@ -1,8 +1,20 @@
 def is_it_possible(field): #"XXXOOOXXX" <= [str]
-    print("GAME:")
     print("", field[:3], "\n", field[3:6], "\n", field[6:])
-    
-    wins = [
+    #
+    #
+    # TEST 1: Counting Pieces & Ex goes first
+    # # # # #
+    sumEx = field.count("X")
+    sumOh = field.count("0")
+    #print(f"Xs: {sumEx}, 0s: {sumOh}")
+    if sumOh > sumEx: print("More 0s than Xs/X plays first")
+    if sumOh > sumEx: return False # Ex always play first / too many Os
+    if sumEx - sumOh > 1: return False  #X has too many pieces, X either has exactly 1 or 0 pieces more
+    #    
+    #
+    # TEST 2: Check win conditions, too many winners
+    # # # # #
+    wins = [ # only looking at X or 0, not the other or blanks
         "ooo......", #Horizontal win, top
         "...ooo...", #Horizontal win, mid
         "......ooo", #Horizontal win, bottom
@@ -14,59 +26,41 @@ def is_it_possible(field): #"XXXOOOXXX" <= [str]
     ]
     
     
+    checkEx = field.replace("X","o").replace("0", ".").replace("_", ".")
+    winEx = 0 #counter 
     
-    
-    # Test 1: Equal number of plays, X goes first (Xs must equal or surpass 0s by 1)
-    # # # # #
-    Oh, Ex = field.count('0'), field.count('X')
+    checkOh = field.replace("0", "o").replace("X", ".").replace("_", ".")
+    winOh = 0
     #
-    if Oh > Ex: return False # Exs equal or surpass Ohs by 1 (X goes first)
-    if Ex - Oh > 1: return False
+    #
+    # IF X/0 wins but has more pieces on the board the win-check FAILS! 
+    # Loop over winning board index. For each 'o', check if a match on the actual gameboard...
+    for win in wins: # iterate over each possible winning condition
+        print("X", checkEx, "|", win, "|", "0", checkOh)
+        if checkEx == win: #CHANGE!
+            print("X won")
+            winEx += 1
+        if checkOh == win:
+            print("0 won")
+            winOh += 1
+    print(winEx, winOh)
+    if winEx > 0 and winOh > 0: return False #both sides CANNOT win
+    if winEx > 2 or winOh > 2: return False # X/0 cannot have more than 2 winning conditions
+    if winEx and sumOh >= sumEx: return False # if X wins, 0 must have 1 less piece
+    if winOh and sumEx > sumOh: return False # if 0 wins, equal number of pieces on board
+    #
+    #
+    #
+    return True # GAME VALID
+    #FIN # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    
+    
+"""
+X/0 CAN win twice, if winning move is at intersection of 2 'ready lines'
+    XXX
+    ..X
+    ..X
+"""
+            
     
         
-    
-    
-    
-    # Test 2: More than one winning triple
-    # # # # #
-    win_count_ex, win_count_oh = 0, 0 #only 1 person can win a game
-    checkfieldEx = field.replace("X","o").replace("0", ".").replace("_", ".")
-    checkfieldOh = field.replace("0", "o").replace("X", ".").replace("_", ".")
-    #print(checkfieldEx)
-    #print(checkfieldOh)
-    print("#")
-    print("", checkfieldOh[:3], "\n", checkfieldOh[3:6], "\n", checkfieldOh[6:])
-    print("#")
-    print("", checkfieldEx[:3], "\n", checkfieldEx[3:6], "\n", checkfieldEx[6:])
-
-
-    
-    for win in wins: #iterate over each winning condition, check against gameboard
-        #print("Checking for wins...")
-        if checkfieldEx == win: #Check all for Ex wins
-            win_count_ex += 1
-            print("X Win:", win)
-        if checkfieldOh == win: #Check for all Oh wins
-            win_count_oh += 1
-            print("0 Win:", win)
-    #
-    #
-    print("X wins:",win_count_ex)
-    print("0 wins:",win_count_oh)
-    if win_count_ex + win_count_oh > 1: return False #Only 1 WIN on the board allowed
-    
-    # When X wins, 0 will always have 1 less mark on the field
-    if win_count_ex == 1 and Ex - Oh != 1: return False
-    #
-    if win_count_oh == 1 and Ex > Oh: return False
-    
-    
-    
-    
-    
-    
-    
-    return True # Passed all tests, so must be a valid game!
-    
-    
-    
