0d565144f2379a0787f62c34f7cb85575c4c6475
MJ <66171297+mjs375@users.noreply.github.com>
2020-11-02 19:52:55 -0500

    Update ***IterativeRotationCipher.py

--- 5kyu/***IterativeRotationCipher.py
+++ 5kyu/***IterativeRotationCipher.py
@@ -1,20 +1,27 @@
 def encode(n,msg):
-	print(n,msg)
-    # 1. Remove all spaces (but remember positions)
-    spaces = [] # a. Add space index in string to a list:
+    
+# 1. Remove all spaces (but remember positions)
+    # a. Add string spaces indices to list:
+    spaces = [] 
     for i, char in enumerate(msg):
-        if i == " ":
-            spaces.append[i]
-    print(spaces, msg)
-    
+        if char == " ":
+            spaces.append(i)
     # b. Remove spaces
+    msg = msg.replace(" ","")
+# 2. Shift order of chars to right by 'n' characters
+    msg = msg[n:] + msg[:n]
+# 3. Put the spaces back in their original positions
+    for i, char in enumerate(msg):
+        if i in spaces:
+            msg = msg[:i] + " " + msg[i:]
+# 4. Shift the characters of each substring (separated by spaces) to the right by 'n'
+    # a. Break string into substrings separated by " "
     
-    
-    # 2. Shift order of chars to right by 'n' characters
-    # 3. Put the spaces back in their original positions
-    # 4. Shift the characters of each substring (separated by spaces) to the right by 'n'
-    # 5. Repeat steps 1-4 'n' times in total. 
-    # 6. Prepend "{n} " to the front of the encoded string.
+    # b. Shift substring by 'n'
+	
+	
+# 5. Repeat steps 1-4 'n' times in total. 
+# 6. Prepend "{n} " to the front of the encoded string.
 	pass
 
 
