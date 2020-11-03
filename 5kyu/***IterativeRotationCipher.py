52daebfba02d8e1565b25e76a75ee75fc15b8307
MJ <66171297+mjs375@users.noreply.github.com>
2020-11-02 19:29:06 -0500

    Create ***IterativeRotationCipher.py

---
+++ 5kyu/***IterativeRotationCipher.py
@@ -0,0 +1,13 @@
+def encode(n,strng):
+	print(n,strng)
+    # 1. Remove all spaces (but remember positions)
+    # 2. Shift order of chars to right by 'n' characters
+    # 3. Put the spaces back in their original positions
+    # 4. Shift the characters of each substring (separated by spaces) to the right by 'n'
+    # 5. Repeat steps 1-4 'n' times in total. 
+    # 6. Prepend "{n} " to the front of the encoded string.
+	pass
+
+def decode(strng):
+	#print(strng)
+	pass
