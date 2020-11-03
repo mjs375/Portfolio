fe01b513bbbe44ffd3842f487aba4ba4c3007f53
MJ <66171297+mjs375@users.noreply.github.com>
2020-11-02 19:31:52 -0500

    Update ***IterativeRotationCipher.py

--- 5kyu/***IterativeRotationCipher.py
+++ 5kyu/***IterativeRotationCipher.py
@@ -1,12 +1,33 @@
-def encode(n,strng):
-	print(n,strng)
+def encode(n,msg):
+	print(n,msg)
     # 1. Remove all spaces (but remember positions)
+    spaces = [] # a. Add space index in string to a list:
+    for i, char in enumerate(msg):
+        if i == " ":
+            spaces.append[i]
+    print(spaces, msg)
+    
+    # b. Remove spaces
+    
+    
     # 2. Shift order of chars to right by 'n' characters
     # 3. Put the spaces back in their original positions
     # 4. Shift the characters of each substring (separated by spaces) to the right by 'n'
     # 5. Repeat steps 1-4 'n' times in total. 
     # 6. Prepend "{n} " to the front of the encoded string.
 	pass
+
+
+
+
+
+
+
+
+
+
+
+
 
 def decode(strng):
 	#print(strng)
