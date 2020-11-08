c469e8500bb8c236e3225cd44669e4af1beddc49
MJ <66171297+mjs375@users.noreply.github.com>
2020-11-08 18:31:00 -0500

    Create WikiArticle_scraper.py

---
+++ Web_Scraping/WikiArticle_scraper.py
@@ -0,0 +1,48 @@
+"""
+Wikipedia Scraping: Pull all Linked Wiki Articles from Article.
+    $ python3 
+"""
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import sys
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+    # ^^ dealing with SSLCertVerificationError
+
+def wikiscraping(url):
+    print("X")
+# # # 1. Open the Page, Prepare Soup:
+    try:
+        page = urlopen(url) # open the webpage
+        html = page.read().decode('utf-8') # converts to HTML, returns a string
+        soup = BeautifulSoup(html, "html.parser") # creates a BeautifulSoup object
+        #print(soup.title.string)
+    except:
+        sys.exit("Bug encountered.")
+
+
+# # # 2. Limit HTMl to div(id="bodyContent")
+    content_soup = soup.find("div", {"id": "bodyContent"})
+    print(content_soup)
+
+
+# # # 3. Search out links, limit to Wiki Article links only: add to dict{}
+    title_link = {}
+    for link in content_soup.find_all("a"):
+        if link.has_attr('href') and link.has_attr('title'):
+            link_url = "https://en.wikipedia.org" + link['href']
+            if ":" not in link['title']:  # only Wiki Articles, not Category pages ("Category:...")
+                title_link[link['title']] = link_url # add to dict{}
+    print(title_link)
+    return title_link
+
+# # # Call the Function:
+wikiscraping("https://en.wikipedia.org/wiki/Codewars")
+#
+
+    
+    
+    
+    
+    
+    
