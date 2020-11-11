e94518b55c1ea3e79b9c75488747098d11a9fd0a
MJ <66171297+mjs375@users.noreply.github.com>
2020-11-10 21:51:28 -0500

    Update ***StringIncrementer.py

--- 5kyu/***StringIncrementer.py
+++ 5kyu/***StringIncrementer.py
@@ -2,19 +2,29 @@ import re
 def increment_string(s):
     print("String:", s)
     num = re.search("[0-9]*$", s) # find numbers on end
-    num = num.group() # now a string
+    num = num.group() # stringify
+    orig = num
+    
+    if num == "": # No number ending string
+        print("no num")
+        return s + "1"
+    
     l = len(re.search("^0*", num).group()) # count leading zeros
-
-    print("Sub:",s[:-l], l)    
-        
+    substring = re.sub(num, "", s) # obtain string w/o numbers
     num = num.lstrip("0") #remove leading zeroes
     print(l, "|", num)
-    if num:
+    if num: # if a trailing number, increment by 1
         num = int(num) +1
         print("New:", num)
     else: # no num, add '1' to end (but also plus any leading zeros back)
-        pass
+        num = 1
         
+    if False:
+        pass
+    else: # Add new number (& leading zeroes) back to substring
+        substring += "0"*l + str(num)
+    return substring
+
     
 """
 .group() :returns the match in the string
