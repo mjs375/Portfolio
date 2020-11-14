ba844d6bd1e79c881d9b3e36604ea3f68df65621
MJ <66171297+mjs375@users.noreply.github.com>
2020-11-14 11:11:50 -0500

    Update tictactoe.py

--- Games/tictactoe.py
+++ Games/tictactoe.py
@@ -1,22 +1,35 @@
-# # # # # # # # # # # # # # # # #
-# # # T I C - T A C - T O E # # #
-# # # # # # # # # # # # # # # # #
+
+                    # # # # # # # # # # # # # # # # #
+                    # # # T I C - T A C - T O E # # #
+                    # # # # # # # # # # # # # # # # #
 """
 Usage: $ python3 tictactoe.py
     - [x] Version 1.0: 2-player mode.
-    - [ ] Version 1.1: 1-player mode, vs. random AI... TODO
+    - [x] Version 1.1: Added (dumb) AI 1-player mode, improved efficiency of check_win f(x)
+    - [ ] Version 1.2: Only check_win for player of that turn (needless inefficiency)
     - [ ] Version 1.2: Improved command-line graphics of board game... TODO
 
 """
 
-import re
-import time
-import sys
+import re           # re.search, re.sub
+import time         # time.sleep
+import sys          # sys.exit, sys.argv
+import random       # random.choice
 
 players = {1:"X", -1:"0"} # dict of turn/players
+AI_moves = ['7','8','9','4','5','6','1','2','3'] # for 1-player
 
-
-
+def game_mode(mode):
+    if mode == "1" or mode == "2":
+        print("mode is good!")
+        return mode
+    while mode != "1" and mode != "2":
+        time.sleep(0.3)
+        mode = input("Enter 1 or 2 (player) as game mode: ")
+    return mode
+#
+#
+#
 # # # Display the gameboard in the console:
 def print_board(board):
     print("", board[:3], "\n", board[3:6], "\n", board[6:])
@@ -29,6 +42,7 @@ def get_move(player, turn, board, move):
     ### Check if legal move (for re-try):
     if move != "%":
         if move in board and move != "X" and move != "0":
+            AI_moves.remove(move) # remove move from AI's next possible moves
             return move
     ### Prompt for move
     move = input(f"What is your move, {player}?\n>>> ")
@@ -36,6 +50,7 @@ def get_move(player, turn, board, move):
     if move == "r": # player(s) can resign(/call a draw) by declaring 'r' move
         sys.exit(f"Player {player} resigned.")
     if move in board and move != "X" and move != "0":
+        AI_moves.remove(move) # remove move from AI's next possible moves
         return move
     else: #re-prompt for move recursively if invalid (until OK)
         time.sleep(0.5)
@@ -46,17 +61,26 @@ def get_move(player, turn, board, move):
         return move
 #
 #
+# # #
+def AI_move():
+    print("Dumb AI is thinking...")
+    time.sleep(0.3)
+    move = random.choice(AI_moves)
+    AI_moves.remove(move) # remove move from AI's next possible moves
+    return move
+#
+#
 #
 # # # Draw valid move on the board
 def draw_move(move, board, turn):
     ### Which player, X or 0?:
-    board = board.replace(move, players.get(turn))
+    board = board.replace(move, players.get(turn)) # replace board num with X/0
     return board
 #
 #
 #
 # # # Did the latest move score the win?
-def win_check(board):
+def win_check(board, turn):
     ### Winning conditions:
     wins = [ # only looking at X OR 0; not the both or blanks
         "ooo......", #Horizontal win, top
@@ -68,23 +92,20 @@ def win_check(board):
         ".o..o..o.", #Vertical win, mid
         "..o..o..o", #Vertical win, R
     ]
-    ### Take a snapshot of the board (turn to X or 0 pieces only):
-    checkEx = board.replace("X", "o").replace("0", ".")
-    checkEx = re.sub("[1-9]", ".", board)
-    checkOh = board.replace("0", "o").replace("X", ".")
-    checkOh = re.sub("[1-9]", ".", board)
-    ###
+    ### Only run tests for player who just went:
+    piece = players.get(turn) # active player piece (e.g. X)
+    other = players.get(turn*-1) # opponent (e.g. 0)
+    check = board.replace(piece,"o").replace(other,".")
+    check = re.sub("[1-9]", ".", check)
+    ### Check each possible win condition against the latest player's 'board':
     win = ""
     for win in wins: # loop over all possible win conditions
         winReg = str(win.replace(".", "[o\.]{1}"))
-        checkRegEx = re.search(str(winReg), str(checkEx))
-        checkRegOh = re.search(str(winReg), str(checkOh))
-        if checkRegEx:
-            return "X"
-        if checkRegOh:
-            return "0"
-        else:
-            return ""
+        checked = re.search(str(winReg), str(check))
+        if checked: #checkRegEx:
+            return piece
+
+    return ""
 
 
 
@@ -92,34 +113,62 @@ def win_check(board):
 # # # Each step in game, looped until game-over:
 def game():
     # # # Starting variables:
+    turn = -1 # global variable
     board = "789456123"
-    turn = -1
     # # # # # LOOP:
     for i in range(9):
         ### Show players the current board:
         if i == 0: # first turn
             print_board(board)
         ### Get active player's move:
-        turn *= -1
+        turn *= -1 # toggle the turn
         player = players.get(turn)
-        move = "%"
-        move = get_move(player, turn, board, move)
+        if mode == "1" and turn == -1: # AI is
+            move = AI_move()
+        else:
+            move = "%"
+            move = get_move(player, turn, board, move)
         ### Edit board:
         board = draw_move(move, board, turn)
         print_board(board)
         time.sleep(0.5)
         ### Check for win conditions:
-        winner = win_check(board)
+        winner = win_check(board, turn)
         ### Check for winner:
-        if winner != "":
-            return f"The winner is {winner}!"
+        if winner == "X" or winner == "0":
+            print(f"The winner is {winner}!")
+            sys.exit()
         ### Rinse & Repeat!
-    sys.exit("It's a draw!")
+    sys.exit("It's a draw!") # All turns over...
 #
 #
 #
-# # # Start the game:
+#
+#
+#
+#
+#
+#
+#
+#
+# # # Get game mode:
+try:
+    print("X",sys.argv[1])
+    mode = game_mode(sys.argv[1])
+except:
+    mode = ""
+    mode = game_mode(mode)
+#
+#
+# # # Start!
+mode = mode # global variable
+
+print(f"Game-mode: {mode}-player.")
+time.sleep(0.3)
 print("Start!")
+time.sleep(0.3)
+
+
 game()
 
 
