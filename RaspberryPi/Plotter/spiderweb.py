d539ad0771376fb1c478907abc2fa8d4feade90a
MJ <66171297+mjs375@users.noreply.github.com>
2020-12-08 13:12:31 -0500

    Update spiderweb.py

--- RaspberryPi/Plotter/spiderweb.py
+++ RaspberryPi/Plotter/spiderweb.py
@@ -1,11 +1,27 @@
 # # # IMPORTS
+#
 from time import sleep 
-  #--control length of pin pulses (on/off, high/low)
+  #--control length of pin pulses (on/off, high/low), &c.
 import sys
   #--
 import RPi.GPIO
   #--control Raspberry PI GPIO pins
 import math # math.sqrt
   #--Calc'ing the belt lengths using PythagTheorem
-import time
-  #--Timing programs/stalling programs...
+
+import spider
+  #--contains the Plotter class
+#
+# # # 
+  
+
+#--Instantiate plotter class instance
+s = spider.Plotter
+  
+
+  
+#
+#
+#--Set SPIDER in motion, get weaving! ... er, drawing!
+for point in path:
+  #
