555d65dd893dfae11251b8e6330229f0a5957293
MJ <66171297+mjs375@users.noreply.github.com>
2020-12-08 13:06:47 -0500

    Update spiderweb.py

--- RaspberryPi/Plotter/spiderweb.py
+++ RaspberryPi/Plotter/spiderweb.py
@@ -1,3 +1,11 @@
 # # # IMPORTS
-
-
+from time import sleep 
+  #--control length of pin pulses (on/off, high/low)
+import sys
+  #--
+import RPi.GPIO
+  #--control Raspberry PI GPIO pins
+import math # math.sqrt
+  #--Calc'ing the belt lengths using PythagTheorem
+import time
+  #--Timing programs/stalling programs...
