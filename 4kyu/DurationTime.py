758d64ec69bf37e108e51392dea8b9f8e87af35a
MJ <66171297+mjs375@users.noreply.github.com>
2020-12-14 18:07:00 -0500

    Update DurationTime.py

--- 4kyu/DurationTime.py
+++ 4kyu/DurationTime.py
@@ -65,4 +65,15 @@ def format_duration(s):
 
 # https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/python
 
-    
+
+"""
+Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+
+The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+
+It is much easier to understand with an example:
+
+format_duration(62)    # returns "1 minute and 2 seconds"
+format_duration(3662)  # returns "1 hour, 1 minute and 2 seconds"
+
+"""
