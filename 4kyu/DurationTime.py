76c69adcd7d66e0d10dc38a3c313ffd2287d6efd
MJ <66171297+mjs375@users.noreply.github.com>
2020-12-14 18:06:16 -0500

    Update and rename ***DurationTime.py to DurationTime.py

--- 4kyu/DurationTime.py
+++ 4kyu/DurationTime.py
@@ -1,5 +1,3 @@
-# https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/python
-
 def format_duration(s):
     if s == 0: return "now"
     m = h = d = y = 0
@@ -9,32 +7,28 @@ def format_duration(s):
     time["hour"] = h #
     time["day"] = d #
     time["year"] = y #
-
     #
-    #  
     # MINUTES:
     if s >= 60:
         m = s // 60
         s = s % 60
         time["minute"] = m #
-        time["second"] = s
-
-        
+        time["second"] = s #
+    #
     # HOURS:
     if m >= 60:
         h = m // 60
         m = m % 60
         time["minute"] = m #
         time["hour"] = h #
-
-        
+    #
     # DAYS
     if h >= 24:
         d = h // 24
         h = h % 24
         time["hour"] = h #
         time["day"] = d #
-            
+    # 
     # YEARS
     if d >= 365:
         y = d // 365
@@ -44,8 +38,31 @@ def format_duration(s):
     #
     #
     # time(dictionary) gives access to both value and key (unit: hour, minute)
+    ans = ""
+    c = 0
+    #--Dictionary is iterated 'reversed', so each string is added to the front, stacking on that way
     for i, v in time.items():
-        print(i,v)
-    print(f"ANS:{time['year']} y" if time["year"] else "TODO")
+        print(i, v)
+        #--If on the second actual value, it will be the 2nd from the end, so end with "and ":
+        if c == 1:
+            if v == 1:
+                c += 1
+                ans = f"{v} {i} and " + ans
+            elif v > 1:
+                c += 1
+                ans = f"{v} {i}s and " + ans
+        #
+        elif v == 1:
+            c += 1
+            ans = f"{v} {i}, " + ans
+        elif v > 1:
+            c += 1
+            ans = f"{v} {i}s, " + ans   
+    #
+    #
+    print(">>> ",ans[:-2]) # strip the last  ", ""
+    return ans[:-2]
+
+# https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/python
+
     
-# f'My name {person["name"]} and my age {person["age"]}'
