ac1cb175143c8e3ca834e0f2a7f62d5bd4e81934
MJ <66171297+mjs375@users.noreply.github.com>
2021-02-21 12:25:11 -0500

    Update Poisonwine.py

--- Games/Poisonwine.py
+++ Games/Poisonwine.py
@@ -1,4 +1,4 @@
-"""
+""" A RIDDLE
 C-3.53 An evil king has n bottles of wine, and a spy has just poisoned one of them. Unfortunately, they do not know which one it is. The poison is very deadly; just one drop diluted even a billion to one will still kill. Even so, it takes a full month for the poison to take effect (you can only do exactly one round of taste-testing). Design a scheme for determining exactly which one of the wine bottles was poisoned in just one month’s time while expending O(logn) taste testers.
 """
 
