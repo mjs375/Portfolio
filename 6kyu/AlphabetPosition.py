9aab9de3d7292ca9a2efb29e38b8850bc6651604
MJ <66171297+mjs375@users.noreply.github.com>
2021-07-17 18:00:36 -0400

    Create AlphabetPosition.py

---
+++ 6kyu/AlphabetPosition.py
@@ -0,0 +1,16 @@
+def alphabet_position(text):
+    positions = ""
+    for char in text:
+        if char.isalpha():
+            a = ord( char.lower() ) - 97 + 1
+            positions += str(a) + " "
+    return positions.strip()
+
+"""
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+If anything in the text isn't a letter, ignore it and don't return it.
+  "a" = 1, "b" = 2, etc.
+Example
+  alphabet_position("The sunset sets at twelve o' clock.")
+    Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+"""
