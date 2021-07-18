c117324178dc9d67749d9ce7f579d1771463f111
MJ <66171297+mjs375@users.noreply.github.com>
2021-07-18 12:33:11 -0400

    Create GetAverage.py

---
+++ 7kyu/GetAverage.py
@@ -0,0 +1,16 @@
+import math
+def new_avg(seq, n):
+    x = n*(len(seq)+1) - sum(seq)
+    if x <= 0: raise Error
+    else: return math.ceil(x)
+    
+"""
+The accounts of the "Fat to Fit Club (FFC)" association are supervised by John as a volunteered accountant. The association is funded through financial donations from generous benefactors. John has a list of the first n donations: [14, 30, 5, 7, 9, 11, 15] He wants to know how much the next benefactor should give to the association so that the average of the first n + 1 donations should reach an average of 30. After doing the math he found 149. He thinks that he made a mistake. Could you help him?
+  if dons = [14, 30, 5, 7, 9, 11, 15] then new_avg(dons, 30) --> 149
+The function new_avg(arr, navg) should return the expected donation (rounded up to the next integer) that will permit to reach the average navg.
+Should the last donation be a non positive number (<= 0) John wants us to throw (or raise) an error
+  new_avg([14, 30, 5, 7, 9, 11, 15], 92) should return 645
+  new_avg([14, 30, 5, 7, 9, 11, 15], 2) 
+    should raise an error (ValueError or invalid_argument or argument-error or DomainError) 
+    or return `-1` or ERROR or Nothing or None depending on the language.
+"""
