afbe52b68a84da36e4a7ef9798ab19503e60769f
MJ <66171297+mjs375@users.noreply.github.com>
2022-01-04 10:47:51 -0500

    Update 1009_ComplementBase10.py

--- Leetcode/Easy/1009_ComplementBase10.py
+++ Leetcode/Easy/1009_ComplementBase10.py
@@ -21,3 +21,39 @@ The complement of an integer is the integer you get when you flip all the 0's to
     Output: 2
       Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2 in base-10.
 """
+
+
+"""
+BITWISE OPERATORS
+
+& (AND:logical conjunction): for each pair of bits occupying the same position, 
+    it returns a '1' only when both bits are switched on.
+            1 0 0 1 1 1 0 0
+            0 0 1 1 0 1 0 0
+        =>  0 0 0 1 0 1 0 0
+    The result is an intersection of the operators' args.
+    Arithmetically, this is equivalent to the product of 2 bit values.
+    
+| (OR:logical disjunction): for each pair of bits,
+    it returns a '1' if at least one is switched on.
+            1 0 0 1 1 1 0 0
+            0 0 1 1 0 1 0 0 
+        =>  1 0 1 1 1 1 0 0
+    The result is a union of the operator's args. Only 2 0's give a '0'.
+    The arithmetic behind this is a combo of a sum and a product of the bit values.
+
+(^) (XOR: exclusive or/exclusive disjunction): simulate it by building atop existing operators.
+            def xor(a,b):
+                return (a and not b) or (not a and b)
+        Evals 2 mutually exclusive conditions and tells you whether 
+        exactly one of them is met. 
+        E.g. a person can be a child or an adult, but not both!
+        It's also not possible for a person to be neither a child nor an adult.
+        - Visually, its a symmetric difference of the operators' args.
+            - 1+0=1   0+0=0   1+1=>0(!)
+~ (NOT: expects just 1 argument): unary bitwise operator. Performs logical negation.
+            1 1 0 0 1 0 1 1
+       => ~ 0 0 1 1 0 1 0 0
+        The inverted bits are a complement to one, which turns 0s=>1s, 1s=>0s.
+ 
+"""
