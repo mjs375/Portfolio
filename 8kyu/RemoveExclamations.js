6130982e916ec2b80479a7d0c94183671d2a5a12
MJ <66171297+mjs375@users.noreply.github.com>
2022-02-04 23:15:58 -0500

    Create RemoveExclamations.js

---
+++ 8kyu/RemoveExclamations.js
@@ -0,0 +1,7 @@
+function remove (string) {
+  return string.replace(/!/g,"") + "!";
+}
+
+/*
+Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+*/
